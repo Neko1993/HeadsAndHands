5f4ff35474c567be7b2f13522b96315d05e6ac8d
Viktoriya <koshka1993@xakep.ru>
2019-10-22 15:47:46 +0300

    File input/output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int a;
+    cin >> a;
+    cout << a;
 
     return 0;
 }
